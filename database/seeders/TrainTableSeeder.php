52617e439c218dd5a57fbf7e5c9e5a9bef26c067
DavideGila <davide.gila@gmail.com>
2023-12-21 16:16:21 +0100

    created seeders for Train with Faker

---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10; $i++) {
+            $newTrain = new Train();
+
+            $newTrain->agency = $faker->company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->dateTime();
+            $newTrain->arrival_time = $faker->dateTime();
+            $newTrain->train_code = $faker->bothify("???-###");
+            $newTrain->carriage_number = $faker->randomNumber(2, true);
+            $newTrain->on_time = $faker->boolean();
+            $newTrain->canceled = $faker->boolean();
+
+            $newTrain->save();
+        }
+    }
+}
